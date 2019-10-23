2a7c6311296aff52aa3c4af86fbdbad50cf89ef8
ljd <helloxiangsheng@163.com>
2019-10-23 09:55:09 +0800

    第一天 初始化项目

---
+++ src/main/java/com/li/hmshop/HmShopApplication.java
@@ -0,0 +1,13 @@
+package com.li.hmshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HmShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/li/hmshop/HmShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.li.hmshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
