60ca8b2dfeb0087b621d0df07b0c1cc013dd45bd
ljd <helloxiangsheng@163.com>
2019-10-28 18:28:30 +0800

    add h2 database

--- shop-admin/src/components/HelloWorld.vue
+++ shop-admin/src/components/HelloWorld.vue
@@ -1,34 +1,29 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>
+    <!-- <h2>
       Ecosystem
       <input type="text" size="8" v-model="nextItemText">
       <button v-on:click.stop="post">{{target}}</button>
     </h2>
-    <todo-list/>
+    
     <ul>
       <li v-for="item in items" v-bind:key="item.id">
         <a href="#">{{item.name}}</a>
       </li>
-    </ul>
+    </ul>-->
   </div>
 </template>
 
 <script>
-import TodoList from './todo/TodoList';
-
 export default {
   name: "HelloWorld",
-  components:{
-    TodoList
-  },
   data() {
     return {
       msg: "Welcome to Your Vue.js App",
       target: "add",
-      nextItemText:'',
-      nextItemId:4,
+      nextItemText: "",
+      nextItemId: 4,
       items: [
         {
           name: "vue-router",
@@ -46,34 +41,10 @@ export default {
       this.items.push({
         name: this.nextItemText,
         id: this.nextItemId++
-      })
+      });
 
-      this.nextItemText = ''
+      this.nextItemText = "";
     }
-  },
-  beforeCreate() {
-    console.log("--- beforeCreate ---");
-  },
-  created() {
-    console.log("--- created ---");
-  },
-  beforeMount() {
-    console.log("--- beforeMount ---");
-  },
-  mounted() {
-    console.log("--- mounted ---");
-  },
-  beforeUpdate() {
-    console.log("--- beforeUpdate ---");
-  },
-  updated() {
-    console.log("--- updated ---");
-  },
-  beforeDestroy() {
-    console.log("--- beforeDestroy ---");
-  },
-  destroyed() {
-    console.log("--- destroyed ---");
   }
 };
 </script>
